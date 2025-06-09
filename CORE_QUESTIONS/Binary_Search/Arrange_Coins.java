ccd8485001b0a350951fab850ae4d0d0c6b8227e
EshaanPadhye2611 <eshaan.padhye@gmail.com>
2025-06-09 14:30:18 +0530

    DSA Java

---
+++ CORE_QUESTIONS/Binary_Search/Arrange_Coins.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        long start = 1;
+        long end = n;
+        while(start <= end){
+            long mid  = start + (end - start) / 2;
+            long total = mid * (mid + 1) / 2;
+            if(total == n){
+                return (int) mid;
+            }else if(total < n){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return (int) end;
+    }
+}
