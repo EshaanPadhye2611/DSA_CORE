d16115406808b1abf5d40a61f866e7308109cc1a
EshaanPadhye2611 <eshaan.padhye@gmail.com>
2025-06-23 13:01:02 +0530

    DSA

---
+++ CORE_QUESTIONS/CardPoints.java
@@ -0,0 +1,18 @@
+\class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+       int sum = 0;
+       int result = Integer.MIN_VALUE;
+       for(int i = 0;i<k;i++){
+        sum += cardPoints[i];
+       }
+       int total = sum;
+       result = sum;
+       for(int i = 1;i<=k;i++){
+        total -= cardPoints[k - i];
+        total += cardPoints[cardPoints.length - i];
+        result = Math.max(result,total);
+        
+       }
+       return result;
+    }
+}
